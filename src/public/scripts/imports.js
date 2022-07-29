8bba5181fc51e0b055f7bc1e029b42143164f385
Artemoha <orhan.mamishov@gmail.com>
2022-07-29 16:20:54 +0300

    create footer + append content

--- src/public/scripts/imports.js
+++ src/public/scripts/imports.js
@@ -1,5 +1,6 @@
 // импорт компонентов
 import "../scripts/components/header";
+import "../scripts/components/footer";
 // импорт стилей
 import "../styles/pages/index/style.scss";
 // импорт скриптов
