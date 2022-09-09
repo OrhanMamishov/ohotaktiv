11d965c119ffeb72c5733c41b14bca035d469e80
Artemoha <orhan.mamishov@gmail.com>
2022-09-09 17:57:30 +0300

    fix

--- src/public/scripts/pages/brands.js
+++ src/public/scripts/pages/brands.js
@@ -1,2 +1,45 @@
 import "../imports";
 import "../../styles/pages/brands/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const brandsStartItems = document.querySelectorAll(".brands__start-item");
+  const brandsInput = document.querySelector(".brands__input");
+  const brandsItem = document.querySelectorAll(".brands__item");
+  brandsStartItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      brandsInput.value = "";
+      brandsStartItems.forEach((el) => el.classList.remove("is-active"));
+      item.classList.add("is-active");
+      if (item.textContent.includes("Все")) {
+        brandsItem.forEach((el) => (el.style.display = "block"));
+        return;
+      }
+      brandsItem.forEach((el) => {
+        const brand = el.children[0].textContent;
+        if (
+          brand
+            .replace(/\s/g, "")
+            .startsWith(item.textContent.replace(/\s/g, ""))
+        ) {
+          el.style.display = "block";
+        } else {
+          el.style.display = "none";
+        }
+      });
+    });
+  });
+  brandsInput.addEventListener("input", (e) => {
+    e.currentTarget.value = e.currentTarget.value.replace(
+      /[^а-я, ^А-Я, a-z, A-Z '']/,
+      ""
+    );
+    brandsItem.forEach((item) => {
+      const brand = item.children[0].textContent;
+      if (brand.toLowerCase().includes(brandsInput.value.toLowerCase())) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
+});
