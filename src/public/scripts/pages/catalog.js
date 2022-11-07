2c5d4b2f36a95f019bf4cba9d7de2f1cf517a533
Artemoha <orhan.mamishov@gmail.com>
2022-11-07 09:06:48 +0300

    some changes

--- src/public/scripts/pages/catalog.js
+++ src/public/scripts/pages/catalog.js
@@ -24,10 +24,6 @@ document.addEventListener("DOMContentLoaded", async () => {
       allowHTML: true,
     });
   });
-  const allAccordions = document.querySelectorAll(".accordion-container");
-  allAccordions.forEach((accordion) => {
-    new Accordion(accordion);
-  });
   const swiperCards = new Swiper(".swiper-cards", {
     modules: [Pagination, Grid],
     spaceBetween: 10,
@@ -178,17 +174,134 @@ document.addEventListener("DOMContentLoaded", async () => {
       }, 200);
     }
   });
-  // const catalogArray = await fetch(
-  //   "https://ohotaktiv.ru/12dev/new-design/pages/catalog/catalog.json",
-  //   {
-  //     method: "GET",
-  //   }
-  // )
-  //   .then((res) => res.json())
-  //   .then((res) => {
-  //     refreshCatalog(res);
-  //   });
+  const catalogHighArray = await fetch(
+    "https://ohotaktiv.ru/12dev/new-design/pages/catalog/sections/menu.json",
+    {
+      method: "GET",
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      return refreshCatalog(res);
+    });
 
+  function refreshCatalog(catalog) {
+    catalog.sort((a, b) => (Number(a.sort) > Number(b.sort) ? 1 : -1));
+    // console.log(catalog);
+    const catalogDownArray = [];
+    catalog.forEach(async (el) => {
+      await fetch(
+        `https://ohotaktiv.ru/12dev/new-design/pages/catalog/sections/${el.file}`,
+        {
+          method: "GET",
+        }
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          return catalogDownArray.push(res);
+        });
+      if (catalog.length == catalogDownArray.length) {
+        while (main.firstChild) {
+          main.removeChild(main.firstChild);
+        }
+        const element = `
+            <section class="catalog">
+              <div class="catalog__wrap container">
+                <nav class="navigation">
+                  <ul class="navigation__list">
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link back"> Назад </a>
+                    </li>
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link"> Главная </a>
+                    </li>
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link"> Каталог </a>
+                    </li>
+                  </ul>
+                </nav>
+                <h1 class="catalog__title">Каталог</h1>
+                <ul class="catalog__list">
+                ${catalog
+                  .map((list) => {
+                    const filteredList = catalogDownArray.filter(
+                      (cat) => cat.catalog[0].section_parent == list.ID
+                    );
+                    // console.log(filteredList);
+                    return `
+                      <li class="catalog__item">
+                        <div class="catalog__img-wrap">
+                          <img
+                            src="${serverName + list.picture}"
+                            alt="${list.name}"
+                            class="catalog__img"
+                          />
+                        </div>
+                        <a href="#" class="catalog__link title"> ${
+                          list.name
+                        } </a>
+                        ${filteredList[0].catalog.section_list
+                          .map((el) => {
+                            console.log(el);
+                            if (el.depth) {
+                              return `
+                                <div class="catalog__accordion accordion-container">
+                                  <div class="ac">
+                                  <h2 class="ac-header">
+                                    <a href="#" class="catalog__link-subtitle"> ${
+                                      el.name
+                                    } </a>
+                                    <button type="button" class="ac-trigger"></button>
+                                  </h2>
+                                  <div class="ac-panel">
+                                    <div class="ac-panel-wrap">
+                                ${el.depth
+                                  .map((depth) => {
+                                    return `
+                                      <a href="#" class="catalog__link"> ${depth.name} </a>
+                                    `;
+                                  })
+                                  .join("")}
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              `;
+                            } else {
+                              return `
+                                <a href="#" class="catalog__link">${el.name}</a>
+                              `;
+                            }
+                          })
+                          .join("")}
+                      </li>
+                      `;
+                  })
+                  .join("")}
+                  </ul>
+                </div>
+              </section>
+          `;
+        main.insertAdjacentHTML("beforeend", element);
+        const allAccordions = document.querySelectorAll(".accordion-container");
+        allAccordions.forEach((accordion) => {
+          new Accordion(accordion);
+        });
+      }
+    });
+
+    // console.log(catalogDown);
+  }
+  //   const thisCatalog = await fetch(
+  //     `https://ohotaktiv.ru/12dev/new-design/pages/catalog/sections/${el.file}`,
+  //     {
+  //       method: "GET",
+  //     }
+  //   )
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       console.log(res);
+  //     });
   // function refreshCatalog(res) {
   //   while (main.firstChild) {
   //     main.removeChild(main.firstChild);
